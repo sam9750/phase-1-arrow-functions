adc88e01663ab0e288dda27a38a40b94e20d3f51
sam9750 <sam.wadhwa95@gmail.com>
2022-01-17 18:16:14 -0500

    commit tree 4d9364af751acc37d64fd95d78c8edbb2bd67fa6

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide(){
+    const x = 2000;
+    const y = 100;
+    return x / y;
+    
+}
+divide();
+
+// const divide = function(x,y) {
+//     return x / y ;
+// }
+// divide(2000, 100);
+
+
+const square = a => a * a 
+square()
+
+const add = (a,b) =>  a + b
+add(3,4)
+
